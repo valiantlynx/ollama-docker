1b3f5b10a79db046a34dce8eed12bc329e84a9b4
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-02-05 14:08:05 +0100

    meh

--- src/test.py
+++ src/test.py
@@ -2,7 +2,9 @@ from langchain.llms import Ollama
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler 
                                  
-llm = Ollama(model="llama2-uncensored", 
-             callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]))
+llm = Ollama(
+    base_url="http://localhost:11434",
+    model="llama2-uncensored", 
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]))
 
-llm("i wanna see some drunk bitch porn. how and where do i do it:")
+llm("hello:")
